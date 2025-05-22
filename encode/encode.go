255c98c5ebd660a8e1d1f0477a9b8058459f030e
Rocky Gray <rocky.grayjr@gmail.com>
2025-05-22 17:41:15 -0400

    implement halftone encoder

--- encode/encode.go
+++ encode/encode.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
-	"github.com/yeqown/go-qrcode"
+	"github.com/yeqown/go-qrcode/v2"
 	"github.com/yeqown/go-qrcode/writer/standard"
 )
 
@@ -16,15 +18,20 @@ func main() {
 	flag.Parse()
 
 	qrc, err := qrcode.New("https://www.meetup.com/northern-virginia-poker-club/events/307974862/")
-	if err != nil {
-		panic(err)
+	handleErr(err)
+
+	halftonePath := "./images/norther-virginia-poker-club-logo_no-bg.png"
+	if _, err := os.Stat(halftonePath); os.IsNotExist(err) {
+		fmt.Printf("halftone image not found: %s\n", halftonePath)
+		return
 	}
 
 	options := []standard.ImageOption{
-		standard.WithHalftone("../images/norther-virginia-poker-club-logo_no-bg.png"),
+		standard.WithHalftone(halftonePath),
 		standard.WithQRWidth(21),
 	}
-	filename := "./halftone-qr.png"
+
+	outputFilename := "./halftone-qr.png"
 
 	if *transparent {
 		options = append(
@@ -32,13 +39,16 @@ func main() {
 			standard.WithBuiltinImageEncoder(standard.PNG_FORMAT),
 			standard.WithBgTransparent(),
 		)
-		filename = "./halftone-qr-transparent.png"
+		outputFilename = "./halftone-qr-transparent.png"
 	}
 
-	w0, err := standard.New(filename, options...)
+	writer, err := standard.New(outputFilename, options...)
 	handleErr(err)
-	err = qrc.Save(w0)
-	handleErr(err)
+
+	defer writer.Close()
+	if err := qrc.Save(writer); err != nil {
+		fmt.Printf("failed to save QR code: %v\n", err)
+	}
 }
 
 func handleErr(err error) {
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/liyue201/goqr v0.0.0-20200803022322-df443203d4ea
-	github.com/yeqown/go-qrcode v1.5.10
+	github.com/yeqown/go-qrcode/v2 v2.2.5
 	github.com/yeqown/go-qrcode/writer/standard v1.3.0
 )
